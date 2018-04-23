436f8fc7bee0a8e25a25e61b1128cb5fa93e39b7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to show the Sort Dialog in a grid in a similar manner as in Excel
+
+
+<p>This example illustrates how to provide an end-user with the Sort Dialog allowing him/her to customize  sorted columns. Also, it is possible to show sort indexes of the sorted columns in their column headers by setting the SortDialogProvider.ShowSortIndexInColumnHeader property to true. </p><p><strong>NOTE: </strong>If you are using version earlier than 10.2 handle the GridView.ShowGridMenu event rather than the  GridView.PopupMenuShowing event in the SortDialogProvider class. </p>
+
+<br/>
+
+
---
+++ VB/WindowsApplication3/SortDialogProvider.vb
@@ -0,0 +1,80 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Text
+Imports DevExpress.XtraGrid.Views.Grid
+Imports DevExpress.XtraGrid.Menu
+Imports DevExpress.Utils.Menu
+Imports System.Windows.Forms
+Imports DevExpress.XtraGrid.Drawing
+Imports DevExpress.Utils.Drawing
+Imports System.Drawing
+Imports DevExpress.XtraGrid.Columns
+Imports WindowsApplication3.Properties
+
+Namespace DXSample
+	 Public Class SortDialogProvider
+		 Private view As GridView
+		 Private showSortIndexInColumnHeader_Renamed As Boolean = True
+		 Public Sub New(ByVal view As GridView)
+			 Me.view = view
+		 End Sub
+
+		 Public Sub EnableSortDialog()
+			 AddHandler view.PopupMenuShowing, AddressOf OnPopupMenuShowing
+			 AddHandler view.CustomDrawColumnHeader, AddressOf OnCustomDrawColumnHeader
+		 End Sub
+
+		 Public Property ShowSortIndexInColumnHeader() As Boolean
+			 Get
+				 Return showSortIndexInColumnHeader_Renamed
+			 End Get
+			 Set(ByVal value As Boolean)
+				 If showSortIndexInColumnHeader_Renamed <> value Then
+					 showSortIndexInColumnHeader_Renamed = value
+					 For Each col As GridColumn In view.SortedColumns
+						 view.InvalidateColumnHeader(col)
+					 Next col
+				 End If
+			 End Set
+		 End Property
+
+		 Private Sub OnCustomDrawColumnHeader(ByVal sender As Object, ByVal e As ColumnHeaderCustomDrawEventArgs)
+			 If (Not ShowSortIndexInColumnHeader) OrElse e.Column Is Nothing OrElse e.Column.SortIndex = -1 Then
+				 Return
+			 End If
+			 Dim args As GridColumnInfoArgs = TryCast(e.Info, GridColumnInfoArgs)
+			 e.Painter.DrawObject(args)
+			 Dim sortArgs As SortedShapeObjectInfoArgs = CType(args.InnerElements.Find(GetType(SortedShapeObjectInfoArgs)).ElementInfo, SortedShapeObjectInfoArgs)
+			 Dim sortRectangle As Rectangle = sortArgs.Bounds
+			 Dim sortIndexRectangle As New Rectangle(sortRectangle.X - 8, args.CaptionRect.Y, 10, args.CaptionRect.Height)
+			 Dim brush As SolidBrush = TryCast(e.Appearance.GetBackBrush(e.Cache), SolidBrush)
+			 brush.Color = Color.FromArgb(150, brush.Color)
+			 Dim sortIndexShapeRectangle As New Rectangle(sortIndexRectangle.X - 2, sortIndexRectangle.Y, sortIndexRectangle.Width + sortRectangle.Width, sortIndexRectangle.Height)
+			 e.Graphics.FillRectangle(brush, sortIndexShapeRectangle)
+			 e.Appearance.DrawString(e.Cache, e.Column.SortIndex.ToString(), sortIndexRectangle)
+			 e.Handled = True
+		 End Sub
+
+		 Private Sub OnPopupMenuShowing(ByVal sender As Object, ByVal e As PopupMenuShowingEventArgs)
+			 Dim menu As GridViewColumnMenu = TryCast(e.Menu, GridViewColumnMenu)
+			 If menu Is Nothing Then
+				 Return
+			 End If
+            menu.Items.Add(New DXMenuItem("Show Sort Dialog", AddressOf OnItemClick, Global.My.Resources.SortDialog))
+		 End Sub
+
+		 Private Sub OnItemClick(ByVal sender As Object, ByVal e As EventArgs)
+			 Using f As New SortForm(view)
+				 If f.ShowDialog() = DialogResult.OK Then
+					 f.SaveChanges()
+				 End If
+			 End Using
+		 End Sub
+
+		 Public Sub DisableSortDialog()
+			 RemoveHandler view.PopupMenuShowing, AddressOf OnPopupMenuShowing
+			 RemoveHandler view.CustomDrawColumnHeader, AddressOf OnCustomDrawColumnHeader
+		 End Sub
+	 End Class
+End Namespace
